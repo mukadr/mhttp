ee5500c6414ea15a9650ef95e9e1826ac555f91a
Mauro Dreissig <mukadr@gmail.com>
2025-12-26 21:58:09 -0300

    request: Break parsing into smaller functions

--- request.c
+++ request.c
@@ -3,14 +3,11 @@
 #include "request.h"
 #include "buffer.h"
 
-static HttpResult parse_method_get_or_head(HttpRequest *request, HttpSlice line, HttpMethod method)
+static HttpResult parse_uri(HttpRequest *request, HttpSlice *line)
 {
-    request->method = method;
-
-    // parse URI
     size_t uri_len = 0;
     while (true) {
-        int c = slice_next(&line);
+        int c = slice_next(line);
         if (c == ' ' || c == '\r' || c == '\n') {
             break;
         }
@@ -24,25 +21,25 @@ static HttpResult parse_method_get_or_head(HttpRequest *request, HttpSlice line,
 
     request->uri[uri_len] = '\0';
 
-    if (!uri_len) {
-        return HTTP_ERROR;
-    }
+    return uri_len ? HTTP_OK : HTTP_ERROR;
+}
 
-    // parse HTTP version
-    if (slice_match(&line, "HTTP/")) {
-        int c = slice_next(&line);
+static HttpResult parse_http_version(HttpRequest *request, HttpSlice *line)
+{
+    if (slice_match(line, "HTTP/")) {
+        int c = slice_next(line);
         if (c < '0' || c > '9') {
             return HTTP_ERROR;
         }
 
         int major = c - '0';
 
-        c = slice_next(&line);
+        c = slice_next(line);
         if (c != '.') {
             return HTTP_ERROR;
         }
 
-        c = slice_next(&line);
+        c = slice_next(line);
         if (c < '0' || c > '9') {
             return HTTP_ERROR;
         }
@@ -52,11 +49,11 @@ static HttpResult parse_method_get_or_head(HttpRequest *request, HttpSlice line,
         request->http_major = major;
         request->http_minor = minor;
 
-        c = slice_next(&line);
+        c = slice_next(line);
         if (c != '\r' && c != '\n' && c != -1) {
             return HTTP_ERROR;
         }
-    } else if (slice_len(&line) && (line.begin[0] == '\r' || line.begin[0] == '\n')) {
+    } else if (slice_len(line) && (line->begin[0] == '\r' || line->begin[0] == '\n')) {
         // HTTP/0.9 (no version present)
         request->http_major = 0;
         request->http_minor = 9;
@@ -67,14 +64,38 @@ static HttpResult parse_method_get_or_head(HttpRequest *request, HttpSlice line,
     return HTTP_OK;
 }
 
+static HttpResult parse_method_get(HttpRequest *request, HttpSlice line)
+{
+    request->method = HTTP_METHOD_GET;
+
+    HttpResult ret = parse_uri(request, &line);
+    if (ret != HTTP_OK) {
+        return ret;
+    }
+
+    return parse_http_version(request, &line);
+}
+
+static HttpResult parse_method_head(HttpRequest *request, HttpSlice line)
+{
+    request->method = HTTP_METHOD_HEAD;
+
+    HttpResult ret = parse_uri(request, &line);
+    if (ret != HTTP_OK) {
+        return ret;
+    }
+
+    return parse_http_version(request, &line);
+}
+
 static HttpResult http_request_parse_method(HttpRequest *request, HttpSlice line)
 {
     if (slice_match(&line, "GET ")) {
-        return parse_method_get_or_head(request, line, HTTP_METHOD_GET);
+        return parse_method_get(request, line);
     }
 
     if (slice_match(&line, "HEAD ")) {
-        return parse_method_get_or_head(request, line, HTTP_METHOD_HEAD);
+        return parse_method_head(request, line);
     }
 
     return HTTP_ERROR;
