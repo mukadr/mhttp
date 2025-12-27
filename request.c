c7f8ba00fafe905cc3a019b1dfff9877607169ed
Mauro Dreissig <mukadr@gmail.com>
2025-12-26 21:43:03 -0300

    request: Move GET parsing code to its own function

--- request.c
+++ request.c
@@ -3,69 +3,74 @@
 #include "request.h"
 #include "buffer.h"
 
+static HttpResult http_request_parse_method_get(HttpRequest *request, HttpSlice line)
+{
+    request->method = HTTP_METHOD_GET;
+
+    // parse URI
+    size_t uri_len = 0;
+    while (true) {
+        int c = slice_next(&line);
+        if (c == ' ' || c == '\r' || c == '\n') {
+            break;
+        }
+
+        request->uri[uri_len++] = (char)c;
+
+        if (uri_len == sizeof(request->uri) - 1) {
+            return HTTP_URI_TOO_LONG;
+        }
+    }
+
+    request->uri[uri_len] = '\0';
+
+    if (!uri_len) {
+        return HTTP_ERROR;
+    }
+
+    // parse HTTP version
+    if (slice_match(&line, "HTTP/")) {
+        int c = slice_next(&line);
+        if (c < '0' || c > '9') {
+            return HTTP_ERROR;
+        }
+
+        int major = c - '0';
+
+        c = slice_next(&line);
+        if (c != '.') {
+            return HTTP_ERROR;
+        }
+
+        c = slice_next(&line);
+        if (c < '0' || c > '9') {
+            return HTTP_ERROR;
+        }
+
+        int minor = c - '0';
+
+        request->http_major = major;
+        request->http_minor = minor;
+
+        c = slice_next(&line);
+        if (c != '\r' && c != '\n' && c != -1) {
+            return HTTP_ERROR;
+        }
+    } else if (slice_len(&line) && (line.begin[0] == '\r' || line.begin[0] == '\n')) {
+        // HTTP/0.9 (no version present)
+        request->http_major = 0;
+        request->http_minor = 9;
+    } else {
+        return HTTP_ERROR;
+    }
+
+    return HTTP_OK;
+}
+
 static HttpResult http_request_parse_method(HttpRequest *request, HttpSlice line)
 {
     if (slice_match(&line, "GET ")) {
-        request->method = HTTP_METHOD_GET;
-
-        // parse URI
-        size_t uri_len = 0;
-        while (true) {
-            int c = slice_next(&line);
-            if (c == ' ' || c == '\r' || c == '\n') {
-                break;
-            }
-
-            request->uri[uri_len++] = (char)c;
-
-            if (uri_len == sizeof(request->uri) - 1) {
-                return HTTP_URI_TOO_LONG;
-            }
-        }
-
-        request->uri[uri_len] = '\0';
-
-        if (!uri_len) {
-            return HTTP_ERROR;
-        }
-
-        // parse HTTP version
-        if (slice_match(&line, "HTTP/")) {
-            int c = slice_next(&line);
-            if (c < '0' || c > '9') {
-                return HTTP_ERROR;
-            }
-
-            int major = c - '0';
-
-            c = slice_next(&line);
-            if (c != '.') {
-                return HTTP_ERROR;
-            }
-
-            c = slice_next(&line);
-            if (c < '0' || c > '9') {
-                return HTTP_ERROR;
-            }
-
-            int minor = c - '0';
-
-            request->http_major = major;
-            request->http_minor = minor;
-
-            c = slice_next(&line);
-            if (c != '\r' && c != '\n' && c != -1) {
-                return HTTP_ERROR;
-            }
-        } else if (slice_len(&line) && (line.begin[0] == '\r' || line.begin[0] == '\n')) {
-            // HTTP/0.9 (no version present)
-            request->http_major = 0;
-            request->http_minor = 9;
-        } else {
-            return HTTP_ERROR;
-        }
-
-        return HTTP_OK;
+        return http_request_parse_method_get(request, line);
     }
 
     return HTTP_ERROR;
