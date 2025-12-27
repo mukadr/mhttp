66d3b8707cb691cb0204c26326a79ec925a36c62
Mauro Dreissig <mukadr@gmail.com>
2025-12-27 19:57:43 -0300

    slice: Expect end of buffer in slice_eol

--- slice.c
+++ slice.c
@@ -20,13 +20,11 @@ bool slice_match(HttpSlice *slice, const char *str)
 
 bool slice_eol(const HttpSlice *slice)
 {
-    if (slice_len(slice) > 0) {
-        if (slice->begin[0] == '\n') {
-            return true;
-        }
-        if (slice_len(slice) > 1 && slice->begin[0] == '\r' && slice->begin[1] == '\n') {
-            return true;
-        }
+    if (slice_len(slice) == 1 && slice->begin[0] == '\n') {
+        return true;
+    }
+    if (slice_len(slice) == 2 && slice->begin[0] == '\r' && slice->begin[1] == '\n') {
+        return true;
     }
     return false;
 }
